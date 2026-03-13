887cb98b88f603ebaf77edcce9850fe6a3df7da8
abaysankar <abaysankar777@gmail.com>
2026-03-13 10:07:56 +0530

    testing ci cd after dir update

--- Backend/src/config/database/db.ts
+++ Backend/src/config/database/db.ts
@@ -10,7 +10,7 @@ export const testDbConnection = async () => {
   try {
     await db.raw("SELECT 1")
 
-    console.log("✅ MySQL connected successfully  // ")
+    console.log("✅ MySQL connected successfully  /// ")
 
   } catch (error) {
     console.error("❌ MySQL connection failed:", error)
